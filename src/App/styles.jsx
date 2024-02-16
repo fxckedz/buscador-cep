49b6636d0c4606b1dfce82c6d66f72658abf124b
MatheusMedeiros <promatheusmedeiros@gmail.com>
2024-02-15 22:55:36 -0300

    Adicionando Responsividade

--- src/App/styles.jsx
+++ src/App/styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { device } from "../styles/device";
 
 export const Screen = styled.div`
   background-color: rgb(240, 242, 245);
@@ -13,25 +14,32 @@ export const Container = styled.div`
   background-color: #1b2327;
   box-shadow: 0px 0px 0px 0.5px #000;
   width: 30%;
-  min-width: 500px;
+  max-width: 500px;
   padding: 2%;
   display: flex;
   flex-direction: column;
   border-radius: 5px;
   align-items: center;
   gap: 20px;
+
+  @media ${device.tablet}{
+    width: 100%;
+    height: 100%;
+    min-width: none;
+    justify-content: center;
+  }
 `;
 
 export const Titulo = styled.h1`
   text-align: center;
   color: #fff;
+  font-size: 2.5rem;
 `;
 
 export const ButtonSearch = styled.div`
   width: 10%;
-  height: 40px;
   display: flex;
-  align-items: center;
+  align-items:center;
   justify-content: center;
   transition: 0.5s;
 
@@ -45,17 +53,20 @@ export const ContainerInput = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
+  border-radius: 5px;
 `;
 
 export const Input = styled.input`
-  background-color: rgb(255,255,255);
+  background-color: rgb(255, 255, 255);
   color: #000;
   outline: none;
   padding-left: 20px;
-  height: 40px;
+  height: 3rem;
   border: none;
-  font-size: 20px;
+  border-radius: 5px;
+  font-size: 30px;
   width: 90%;
+
 `;
 
 export const ContainerInfo = styled.div`
@@ -65,9 +76,11 @@ export const ContainerInfo = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background-color: rgb(255,255,255);
+  background-color: rgb(255, 255, 255);
   border-radius: 5px;
   padding: 10px;
-  gap: 10px;
+  gap: 15px;
   font-weight: 600;
+  font-size: 1rem;
+
 `;
